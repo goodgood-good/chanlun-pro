ce460ed31a66d26de01a7aca0b84b66907cef77f
lc <393768225@qq.com>
2026-06-16 10:51:39 +0800

    refactor(strategy_optimizer): 抽 scoring 评分层(Phase4 第4批)

--- src/chanlun/recursive_bt/strategy_optimizer/__init__.py
+++ src/chanlun/recursive_bt/strategy_optimizer/__init__.py
@@ -7,4 +7,5 @@
 """
 from chanlun.recursive_bt.strategy_optimizer.models import *  # noqa: F401,F403
 from chanlun.recursive_bt.strategy_optimizer.candidates import *  # noqa: F401,F403
+from chanlun.recursive_bt.strategy_optimizer.scoring import *  # noqa: F401,F403
 from chanlun.recursive_bt.strategy_optimizer._impl import *  # noqa: F401,F403
